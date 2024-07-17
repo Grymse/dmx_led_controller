bfa8169150028d51b074c858d8e9bb64837cb213
Nicolai Grymer <pronicoxd@gmail.com>
2024-07-17 15:54:52 +0200

    Fix natural color ordering in setup

--- src/main.cpp
+++ src/main.cpp
@@ -82,7 +82,7 @@ void setup() {
       debug("radio hardware is not responding!!", 0);
     }  // hold in infinite loop
   }
-  FastLED.addLeds<WS2812B, LED_PIN, RGB>(leds, num_leds_in_strip);
+  FastLED.addLeds<WS2812B, LED_PIN, GRB>(leds, num_leds_in_strip);
   //ws2812B 4pin sorte full cover
   //ws2811 til hvid strip hvid tape.  
 
