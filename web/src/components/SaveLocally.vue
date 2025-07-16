047f339499ec84bf8a280f4ea790a45c046c74b0
Jakob Houmøller Pedersen <1546-au649483@users.noreply.gitlab.au.dk>
2025-07-16 14:23:50 +0200

    Name a program when u save it.

--- web/src/components/SaveLocally.vue
+++ web/src/components/SaveLocally.vue
@@ -6,7 +6,7 @@
       icon="pi pi-download"
       severity="secondary"
       outlined
-      @click="saveSequence"
+      @click="openSaveDialog"
     />
     <Button
       label="Load Program"
@@ -16,12 +16,25 @@
       @click="loadSequence"
     />
     <input type="file" id="fileInput" @change="handleFileChange" style="display: none" accept=".json">
+
+    <Dialog v-model:visible="saveDialogVisible" modal header="Save Sequence" :style="{ width: '300px' }">
+      <div class="flex flex-col gap-1 overflow-auto">
+        <label for="filename">Filename:</label>
+        <InputText id="filename" v-model="filename" type="text" />
+      </div>
+      <template #footer>
+        <Button label="Cancel" icon="pi pi-times" @click="saveDialogVisible = false" class="p-button-text" />
+        <Button label="Save" icon="pi pi-check" @click="saveSequence" autofocus />
+      </template>
+    </Dialog>
   </div>
 </template>
 
 <script setup lang="ts">
-import { defineProps, defineEmits } from 'vue';
+import { ref, defineProps, defineEmits } from 'vue';
 import Button from 'primevue/button';
+import Dialog from 'primevue/dialog';
+import InputText from 'primevue/inputtext';
 import type { Module } from '@/lib/module';
 
 const props = defineProps({
@@ -33,6 +46,13 @@ const props = defineProps({
 
 const emit = defineEmits(['updateSequence']);
 
+const filename = ref('sequence');
+const saveDialogVisible = ref(false);
+
+const openSaveDialog = () => {
+  saveDialogVisible.value = true;
+};
+
 const saveSequence = () => {
   // Convert the modules array to a JSON string
   const jsonData = JSON.stringify(props.currentSequence, null, 2);
@@ -40,11 +60,12 @@ const saveSequence = () => {
   const url = URL.createObjectURL(blob);
   const a = document.createElement('a');
   a.href = url;
-  a.download = 'sequence.json';
+  a.download = filename.value + '.json';
   document.body.appendChild(a);
   a.click();
   document.body.removeChild(a);
   URL.revokeObjectURL(url);
+  saveDialogVisible.value = false;
 };
 
 const loadSequence = () => {
